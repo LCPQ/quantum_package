20ed8c923a05f82f7251bc045242e541d79b4050
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2014-04-03 11:28:54 +0200

    Simplified Makefiles

--- scripts/create_Makefile.sh
+++ scripts/create_Makefile.sh
@@ -30,7 +30,7 @@ EOF
 cat << EOF > Makefile
 default: all
 
-NEEDED_MODULES=$(shell cat NEEDED_MODULES)
+NEEDED_MODULES=\$(shell cat NEEDED_MODULES)
 
 # Define here all new external source files and objects.Don't forget to prefix the
 # object files with IRPF90_temp/
@@ -45,7 +45,7 @@ include irpf90.make
 irpf90.make: \$(filter-out IRPF90_temp/%, \$(wildcard */*.irp.f)) \$(wildcard *.irp.f) \$(wildcard *.inc.f) Makefile \$(EZFIO) NEEDED_MODULES
 	\$(IRPF90)
 
-Makefile.depend: Makefile
+Makefile.depend: NEEDED_MODULES
 	\$(QPACKAGE_ROOT)/scripts/create_Makefile_depend.sh
 EOF
 
