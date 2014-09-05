17c71c316288ef3de2ef0090ae5c25538bcc4a3a
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2014-09-06 01:01:44 +0200

    Selectors and generators default -> 0.999 of norm

--- src/Selectors_full/selectors.irp.f
+++ src/Selectors_full/selectors.irp.f
@@ -1,21 +1,16 @@
 use bitmasks
 
-BEGIN_PROVIDER [ double precision, threshold_selectors ]
- implicit none
- BEGIN_DOC
- ! Percentage of the norm of the state-averaged wave function to
- ! consider for the selectors
- END_DOC
- logical                        :: exists
- PROVIDE ezfio_filename
- call ezfio_has_determinants_threshold_selectors(exists)
- if (exists) then
-   call ezfio_get_determinants_threshold_selectors(threshold_selectors)
- else
-   threshold_selectors = 0.99d0
- endif
- call write_double(output_Dets,threshold_selectors,'Threshold on selectors')
-END_PROVIDER
+BEGIN_SHELL [ /usr/bin/python ]
+from ezfio_with_default import EZFIO_Provider
+T = EZFIO_Provider()
+T.set_type      ( "double precision" )
+T.set_name      ( "threshold_selectors" )
+T.set_doc       ( "Percentage of the norm of the state-averaged wave function to consider for the selectors" )
+T.set_ezfio_dir ( "determinants" )
+T.set_ezfio_name( "threshold_selectors" )
+T.set_output    ( "output_dets" )
+print T
+END_SHELL
 
 BEGIN_PROVIDER [ integer, psi_selectors_size ]
  implicit none
