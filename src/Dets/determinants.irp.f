66ab8d5e2a88a83bd70de63c070700d716391f79
Manu <emmanuel_giner_jr@hotmail.fr>
2014-11-13 10:43:26 +0100

    Bug in bit_tmp

--- src/Dets/determinants.irp.f
+++ src/Dets/determinants.irp.f
@@ -133,7 +133,9 @@ END_PROVIDER
    !$DIR FORCEINLINE
    bit_tmp(i) = occ_pattern_search_key(psi_occ_pattern(1,1,i),N_int)
  enddo
+ print*,'passed 1'
  call i8sort(bit_tmp,iorder,N_det)
+ print*,'passed 2'
  !DIR$ IVDEP
  do i=1,N_det
   do k=1,N_int
@@ -149,7 +151,7 @@ END_PROVIDER
   duplicate(i) = .False.
  enddo
 
- do i=1,N_det
+ do i=1,N_det-1
   if (duplicate(i)) then
     cycle
   endif
@@ -173,6 +175,7 @@ END_PROVIDER
     endif
   enddo
  enddo
+  print*,'passed 3'
 
  N_occ_pattern=0
  do i=1,N_det
