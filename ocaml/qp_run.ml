555fba503e4bb6e46acb7a0409ac4a0e3d98aa7f
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2015-01-10 00:28:44 +0100

    Repaired qp_run

--- ocaml/qp_run.ml
+++ ocaml/qp_run.ml
@@ -20,27 +20,6 @@ let run exe ezfio_file =
   Printf.printf "===============\nQuantum Package\n===============\n\n";
   Printf.printf "Date : %s\n\n%!" (Time.to_string time_start);
 
-  let output_dir = ezfio_file^"/output" in
-  if (Sys.file_exists_exn output_dir) then
-    begin
-      Sys.ls_dir output_dir
-      |> List.iter ~f:(fun x -> Sys.remove (output_dir^"/"^x));
-      Unix.rmdir output_dir
-    end;
-  
-  let fifo_name = ezfio_file^"/.fifo" in
-  if (Sys.file_exists_exn fifo_name) then
-    Sys.remove fifo_name;
-  Unix.mkfifo ~perm:0o664 fifo_name;
-
-  let pid = 
-    In_channel.with_file fifo_name ~f:(fun in_channel ->
-      In_channel.input_all in_channel |> String.strip )
-    |> Int.of_string
-    |> Pid.of_int
-  in
-  Sys.remove fifo_name;
-
   let exe =
     match (List.find ~f:(fun (x,_) -> x = exe) executables) with
     | None -> assert false
@@ -50,13 +29,6 @@ let run exe ezfio_file =
   | 0 -> ()
   | i -> Printf.printf "Program exited with code %d.\n%!" i;
   ;
-
-  Signal.send_exn (Signal.of_system_int 2) (`Pid pid);
-
-
-
-  (* Run the executable in the foreground
-   * ==================================== *)
 
   let duration = Time.diff (Time.now()) time_start 
   |> Core.Span.to_string in
