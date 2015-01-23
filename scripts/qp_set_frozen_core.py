62bdc4948a6c19332167c63f84c79adb46660aab
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2015-01-23 17:32:31 +0100

    Added qp_set_frozen_core.py

---
+++ scripts/qp_set_frozen_core.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path = [ os.environ["QPACKAGE_ROOT"]+"/EZFIO/Python" ] + sys.path
+from ezfio import ezfio
+
+ezfio.set_filename(sys.argv[1])
+
+nb = 0
+for charge in ezfio.nuclei_nucl_charge:
+   if charge < 5:
+      pass
+   elif charge < 13:
+      nb += 1
+   else:
+      nb += 5
+
+mo_tot_num = ezfio.mo_basis_mo_tot_num
+
+os.system( """qp_set_mo_class -core "[1-%d]" -act "[%d-%d]" %s"""%(nb, nb+1, mo_tot_num, sys.argv[1]) )
+
+
