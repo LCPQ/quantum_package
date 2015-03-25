29143cbe0a1c7091ae05646762b097a6bafc3dba
Manu <emmanuel_giner_jr@hotmail.fr>
2015-03-25 12:06:50 +0100

    Fixed bugs of DDCI

--- src/Full_CI/H_apply.irp.f
+++ src/Full_CI/H_apply.irp.f
@@ -11,22 +11,21 @@ s.set_perturbation("epstein_nesbet_2x2")
 print s
 
 
-if False:
-  s = H_apply("FCI_mono")
-  s.set_selection_pt2("epstein_nesbet_2x2")
-  s.unset_double_excitations()
-  print s
+s = H_apply("FCI_mono")
+s.set_selection_pt2("epstein_nesbet_2x2")
+s.unset_double_excitations()
+print s
 
 
-  s = H_apply("select_mono_delta_rho")
-  s.unset_double_excitations()
-  s.set_selection_pt2("delta_rho_one_point")
-  print s
+s = H_apply("select_mono_delta_rho")
+s.unset_double_excitations()
+s.set_selection_pt2("delta_rho_one_point")
+print s
 
-  s = H_apply("pt2_mono_delta_rho")
-  s.unset_double_excitations()
-  s.set_perturbation("delta_rho_one_point")
-  print s
+s = H_apply("pt2_mono_delta_rho")
+s.unset_double_excitations()
+s.set_perturbation("delta_rho_one_point")
+print s
 
 s = H_apply("select_mono_di_delta_rho")
 s.set_selection_pt2("delta_rho_one_point")
