e9665c276ca124ae8574b48248e90ca3a06d5f11
Thomas Applencourt <t.applencourt@gmail.com>
2015-03-30 17:56:17 +0200

    Add cisd_sc2_selected to qp_edit

--- ocaml/Input.ml
+++ ocaml/Input.ml
@@ -7,11 +7,11 @@ include Input_ao_basis;;
 include Input_bi_integrals;;
 include Input_bitmasks;;
 include Input_cisd_sc2;;
+include Input_cisd_sc2_selected;;
 include Input_determinants;;
 include Input_electrons;;
 include Input_full_ci;;
 include Input_hartree_fock;;
 include Input_mo_basis;;
 include Input_nuclei;;
-
 
