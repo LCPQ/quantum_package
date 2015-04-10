5962aff7eeee0b08d9813b1817d733fb28d2c20a
Thomas Applencourt <t.applencourt@gmail.com>
2015-04-10 16:51:46 +0200

    mend
    Beter automisation for put_pseudo_in_ezfio.py

--- scripts/pseudo/create_ez.sh
+++ scripts/pseudo/create_ez.sh
@@ -3,9 +3,10 @@
 # $2 mult
 
 echo "name" $1
-echo "mul"  $2
+echo "basis" $2
+echo "mul"  $3
 echo "\`get_basis.sh\` need to be changed"
 
 rm -R $1.ezfio 
-qp_create_ezfio_from_xyz $1.xyz -b "cc-pvdz" -m $2
-~/quantum_package/scripts/pseudo/put_pseudo_in_ezfio.py --ezfio $1.ezfio
+qp_create_ezfio_from_xyz $1.xyz -b $2 -m $3
+~/quantum_package/scripts/pseudo/put_pseudo_in_ezfio.py $1.ezfio
