5dc7810bdec377939bf79f9e7ff3bcd869696730
TApplencourt <tapplencourt@gmail.com>
2015-09-30 13:19:45 +0200

    Fix ezfio path and ocaml version

--- install/scripts/install_ocaml.sh
+++ install/scripts/install_ocaml.sh
@@ -7,9 +7,18 @@ cd -
 # Normal installation
 PACKAGES="core cryptokit ocamlfind sexplib"
 
-declare -i i
-i=$(gcc -dumpversion | cut -d '.' -f 2)
-if [[ i -lt 6 ]]
+# return 0 if program version is equal or greater than check version
+check_version()
+{
+    local version=$1 check=$2
+    local winner=$(echo -e "$version\n$check" | sed '/^$/d' | sort -nr | head -1)
+    [[ "$winner" = "$version" ]] && return 0
+    return 1
+}
+
+i=$(gcc -dumpversion)
+
+if check_version i 4.6
 then
    echo "GCC version $(gcc -dumpversion) too old. GCC >= 4.6 required."
    exit 1
