6cf3dcca0b00c9ee619a267bb8bb91a84fa73db7
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2015-11-24 11:40:49 +0100

    Extended template for pt2

--- plugins/Perturbation/perturbation.template.f
+++ plugins/Perturbation/perturbation.template.f
@@ -43,24 +43,8 @@ subroutine perturb_buffer_$PERT(i_generator,buffer,buffer_size,e_2_pert_buffer,c
   end if
   
   
-  buffer_loop : do i = 1,buffer_size
+  do i=1,buffer_size
     
-!     do k=1,N_minilist_gen
-!       ex = 0
-!       do ni=1,Nint
-!         ex += popcnt(xor(minilist_gen(ni,1,k), buffer(ni,1,i))) + popcnt(xor(minilist_gen(ni,2,k), buffer(ni,2,i)))
-!       end do
-!       if(ex <= 4) then
-!         cycle buffer_loop
-!       end if
-!     end do
-    
-!     c_ref = connected_to_ref(buffer(1,1,i),miniList_gen,Nint,N_minilist_gen+1,N_minilist_gen)
-! 
-!     if (c_ref /= 0) then
-!       cycle
-!     endif
-
     if(is_connected_to(buffer(1,1,i), miniList_gen, Nint, N_minilist_gen)) then
       cycle
     end if
@@ -71,20 +55,18 @@ subroutine perturb_buffer_$PERT(i_generator,buffer,buffer_size,e_2_pert_buffer,c
     
     integer :: degree
     call get_excitation_degree(HF_bitmask,buffer(1,1,i),degree,N_int)
-!     call pt2_$PERT(buffer(1,1,i),         &
-!         c_pert,e_2_pert,H_pert_diag,Nint,N_det_selectors,n_st,minilist,idx_minilist)
     call pt2_$PERT(buffer(1,1,i),         &
-         c_pert,e_2_pert,H_pert_diag,Nint,N_minilist,n_st,minilist,idx_minilist,N_minilist) !!!!!!!!!!!!!!!!! MAUVAISE SIGNATURE PR LES AUTRES PT2_* !!!!!
+         c_pert,e_2_pert,H_pert_diag,Nint,N_minilist,n_st,minilist,idx_minilist,N_minilist) 
 
     do k = 1,N_st
-      e_2_pert_buffer(k,i) = e_2_pert(k)
-      coef_pert_buffer(k,i) = c_pert(k)
-      sum_norm_pert(k) += c_pert(k) * c_pert(k)
-      sum_e_2_pert(k) += e_2_pert(k)
-      sum_H_pert_diag(k) +=  H_pert_diag(k)
+      e_2_pert_buffer(k,i)   = e_2_pert(k)
+      coef_pert_buffer(k,i)  = c_pert(k)
+      sum_norm_pert(k)       = sum_norm_pert(k)   + c_pert(k) * c_pert(k)
+      sum_e_2_pert(k)        = sum_e_2_pert(k)    + e_2_pert(k)
+      sum_H_pert_diag(k)     = sum_H_pert_diag(k) + H_pert_diag(k)
     enddo
     
-  enddo buffer_loop
+  enddo 
   
 end
 
