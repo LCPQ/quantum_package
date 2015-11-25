6cad30269f44890ba3506009098475b7caa5f409
Michel Caffarel <caffarel@eoslogin2.bullx>
2015-11-25 18:04:05 +0100

    Guess based on the overlap matrix of AOs to remove linear dependencies

--- src/MO_Basis/utils.irp.f
+++ src/MO_Basis/utils.irp.f
@@ -21,6 +21,29 @@ subroutine save_mos
   
 end
 
+subroutine save_mos_truncated(n)
+  implicit none
+  double precision, allocatable  :: buffer(:,:)
+  integer                        :: i,j,n
+  
+  call system('$QP_ROOT/scripts/save_current_mos.sh '//trim(ezfio_filename))
+  
+  call ezfio_set_mo_basis_mo_tot_num(n)
+  call ezfio_set_mo_basis_mo_label(mo_label)
+  call ezfio_set_mo_basis_ao_md5(ao_md5)
+  allocate ( buffer(ao_num,n) )
+  buffer = 0.d0
+  do j = 1, n
+    do i = 1, ao_num
+      buffer(i,j) = mo_coef(i,j)
+    enddo
+  enddo
+  call ezfio_set_mo_basis_mo_coef(buffer)
+  call ezfio_set_mo_basis_mo_occ(mo_occ)
+  deallocate (buffer)
+  
+end
+
 subroutine mo_as_eigvectors_of_mo_matrix(matrix,n,m,label)
   implicit none
   integer,intent(in)             :: n,m
