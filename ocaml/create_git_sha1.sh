f51269dd79c60a6298c2f4f7ab4b18d0f8ad25fa
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2015-12-08 14:40:23 +0100

    Corrected date in Git.ml

--- ocaml/create_git_sha1.sh
+++ ocaml/create_git_sha1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SHA1=$(git log -1  | head -1 | cut -d ' ' -f 2)
-DATE=$(git log -1  | grep Date | cut -d ':' -f 2)
+DATE=$(git log -1  | grep Date | cut -d ':' -f 2-)
 MESSAGE=$(git log -1  | tail -1)
 cat << EOF > Git.ml
 open Core.Std
