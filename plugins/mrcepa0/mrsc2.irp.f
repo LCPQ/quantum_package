cdb87937c868a685f490ca825c2f922a7527f7d7
Yann Garniron <yann.garniron@yahoo.fr>
2016-04-01 23:31:48 +0200

    added missing files

---
+++ plugins/mrcepa0/mrsc2.irp.f
@@ -0,0 +1,26 @@
+program mrcepa0
+  implicit none
+  !mrmode : 1=mrcepa0, 2=mrsc2 add, 3=mrsc2 sub
+  mrmode = 2
+  if (.not.read_wf) then
+    print *,  'read_wf has to be true.'
+    stop 1
+  endif
+  call print_cas_coefs
+  call run_mrcepa0
+end
+
+subroutine print_cas_coefs
+  implicit none
+
+  integer :: i,j
+  print *,  'CAS'
+  print *,  '==='
+  do i=1,N_det_cas
+    print *,  psi_cas_coef(i,:)
+    call debug_det(psi_cas(1,1,i),N_int)
+  enddo
+
+  call write_double(6,ci_energy(1),"Initial CI energy")
+end
+
