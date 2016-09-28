0b769ccf32537d8b217d54427a8cabcb115948c7
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2016-09-28 20:27:59 +0200

    EZFIO I/O for 1-e integrals

--- src/Integrals_Monoelec/ao_overlap.irp.f
+++ src/Integrals_Monoelec/ao_overlap.irp.f
@@ -14,51 +14,61 @@
   double precision :: alpha, beta, c
   double precision :: A_center(3), B_center(3)
   integer :: power_A(3), power_B(3)
-  dim1=100
-  !$OMP PARALLEL DO SCHEDULE(GUIDED) &
-  !$OMP DEFAULT(NONE) &
-  !$OMP PRIVATE(A_center,B_center,power_A,power_B,&
-  !$OMP  overlap_x,overlap_y, overlap_z, overlap, &
-  !$OMP  alpha, beta,i,j,c) &
-  !$OMP SHARED(nucl_coord,ao_power,ao_prim_num, &
-  !$OMP  ao_overlap_x,ao_overlap_y,ao_overlap_z,ao_overlap,ao_num,ao_coef_normalized_ordered_transp,ao_nucl, &
-  !$OMP  ao_expo_ordered_transp,dim1)
-  do j=1,ao_num
-   A_center(1) = nucl_coord( ao_nucl(j), 1 )
-   A_center(2) = nucl_coord( ao_nucl(j), 2 )
-   A_center(3) = nucl_coord( ao_nucl(j), 3 )
-   power_A(1)  = ao_power( j, 1 )
-   power_A(2)  = ao_power( j, 2 )
-   power_A(3)  = ao_power( j, 3 )
-   !DEC$ VECTOR ALIGNED
-   !DEC$ VECTOR ALWAYS
-   do i= 1,ao_num
-    ao_overlap(i,j)= 0.d0
-    ao_overlap_x(i,j)= 0.d0
-    ao_overlap_y(i,j)= 0.d0
-    ao_overlap_z(i,j)= 0.d0
-    B_center(1) = nucl_coord( ao_nucl(i), 1 )
-    B_center(2) = nucl_coord( ao_nucl(i), 2 )
-    B_center(3) = nucl_coord( ao_nucl(i), 3 )
-    power_B(1)  = ao_power( i, 1 )
-    power_B(2)  = ao_power( i, 2 )
-    power_B(3)  = ao_power( i, 3 )
-    do n = 1,ao_prim_num(j)
-     alpha = ao_expo_ordered_transp(n,j)
-     !DEC$ VECTOR ALIGNED
-     do l = 1, ao_prim_num(i)
-      beta = ao_expo_ordered_transp(l,i)
-      call overlap_gaussian_xyz(A_center,B_center,alpha,beta,power_A,power_B,overlap_x,overlap_y,overlap_z,overlap,dim1)
-      c = ao_coef_normalized_ordered_transp(n,j) * ao_coef_normalized_ordered_transp(l,i)
-      ao_overlap(i,j) += c * overlap
-      ao_overlap_x(i,j) += c * overlap_x
-      ao_overlap_y(i,j) += c * overlap_y
-      ao_overlap_z(i,j) += c * overlap_z
-     enddo
+   if (read_ao_one_integrals) then
+     call ezfio_get_ao_basis_integral_overlap(ao_overlap(1:ao_num, 1:ao_num))
+ call ezfio_set_ao_basis_integral_overlap(ao_overlap(1:ao_num, 1:ao_num))
+     print *,  'AO overlap integrals read from disk'
+  else
+    dim1=100
+    !$OMP PARALLEL DO SCHEDULE(GUIDED) &
+    !$OMP DEFAULT(NONE) &
+    !$OMP PRIVATE(A_center,B_center,power_A,power_B,&
+    !$OMP  overlap_x,overlap_y, overlap_z, overlap, &
+    !$OMP  alpha, beta,i,j,c) &
+    !$OMP SHARED(nucl_coord,ao_power,ao_prim_num, &
+    !$OMP  ao_overlap_x,ao_overlap_y,ao_overlap_z,ao_overlap,ao_num,ao_coef_normalized_ordered_transp,ao_nucl, &
+    !$OMP  ao_expo_ordered_transp,dim1)
+    do j=1,ao_num
+    A_center(1) = nucl_coord( ao_nucl(j), 1 )
+    A_center(2) = nucl_coord( ao_nucl(j), 2 )
+    A_center(3) = nucl_coord( ao_nucl(j), 3 )
+    power_A(1)  = ao_power( j, 1 )
+    power_A(2)  = ao_power( j, 2 )
+    power_A(3)  = ao_power( j, 3 )
+    !DEC$ VECTOR ALIGNED
+    !DEC$ VECTOR ALWAYS
+    do i= 1,ao_num
+      ao_overlap(i,j)= 0.d0
+      ao_overlap_x(i,j)= 0.d0
+      ao_overlap_y(i,j)= 0.d0
+      ao_overlap_z(i,j)= 0.d0
+      B_center(1) = nucl_coord( ao_nucl(i), 1 )
+      B_center(2) = nucl_coord( ao_nucl(i), 2 )
+      B_center(3) = nucl_coord( ao_nucl(i), 3 )
+      power_B(1)  = ao_power( i, 1 )
+      power_B(2)  = ao_power( i, 2 )
+      power_B(3)  = ao_power( i, 3 )
+      do n = 1,ao_prim_num(j)
+      alpha = ao_expo_ordered_transp(n,j)
+      !DEC$ VECTOR ALIGNED
+      do l = 1, ao_prim_num(i)
+        beta = ao_expo_ordered_transp(l,i)
+        call overlap_gaussian_xyz(A_center,B_center,alpha,beta,power_A,power_B,overlap_x,overlap_y,overlap_z,overlap,dim1)
+        c = ao_coef_normalized_ordered_transp(n,j) * ao_coef_normalized_ordered_transp(l,i)
+        ao_overlap(i,j) += c * overlap
+        ao_overlap_x(i,j) += c * overlap_x
+        ao_overlap_y(i,j) += c * overlap_y
+        ao_overlap_z(i,j) += c * overlap_z
+      enddo
+      enddo
     enddo
-   enddo
-  enddo
-  !$OMP END PARALLEL DO
+    enddo
+    !$OMP END PARALLEL DO
+  endif
+  if (write_ao_one_integrals) then
+     call ezfio_set_ao_basis_integral_overlap(ao_overlap(1:ao_num, 1:ao_num))
+     print *,  'AO overlap integrals written to disk'
+  endif
 
 END_PROVIDER
 
