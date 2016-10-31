6ebfa85f0f1c8faf16b6914ded9204f048c35d3a
Thomas Applencourt <TApplencourt@users.noreply.github.com>
2016-10-31 15:05:57 -0500

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
 LIST="
-
 convert.bats
 hf.bats
 foboci.bats
@@ -9,28 +8,29 @@ pseudo.bats
 fci.bats
 cassd.bats
 mrcepa0.bats
-
 "
+
 
 export QP_PREFIX="timeout -s 9 300"
 export QP_TASK_DEBUG=1
+
 rm -rf work output
 
-if [[ "$1" == "-v" ]]
-then
-  for BATS_FILE in $LIST
-  do
-    echo
-    echo "-~-~-~-~-~-~"
-    echo
-    echo "Running tests for ${BATS_FILE%.bats}"
-    echo
-    BATS_FILE=bats/$BATS_FILE
+
+for BATS_FILE in $LIST
+do
+  echo
+  echo "-~-~-~-~-~-~"
+  echo
+  echo "Running tests for ${BATS_FILE%.bats}"
+  echo
+  BATS_FILE=bats/$BATS_FILE
+  if [[ "$1" == "-v" ]]
+  then
     echo "Verbose mode"
-    ./bats_to_sh.py $BATS_FILE | bash
-   done
-else
-   cd bats
-   bats .
-fi
+    ./bats_to_sh.py $BATS_FILE | bash 
+  else
+    bats $BATS_FILE
+  fi
+done
 
