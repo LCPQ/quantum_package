a0736ce4e1af0a9562f8773dc85c7c9f32f02b76
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2017-03-06 17:32:42 +0100

    Fixed OCaml Address in use

--- ocaml/TaskServer.ml
+++ ocaml/TaskServer.ml
@@ -62,7 +62,11 @@ let bind_socket ~socket_type ~socket ~port =
       | Unix.Unix_error _ -> (Time.pause @@ Time.Span.of_float 1. ; loop (i-1) )
       | other_exception -> raise other_exception
   in loop 60;
-  ZMQ.Socket.bind socket @@ Printf.sprintf "ipc:///tmp/qp_run:%d" port
+  let filename =
+    Printf.sprintf "/tmp/qp_run:%d" port
+  in
+  Sys.remove filename;
+  ZMQ.Socket.bind socket ("ipc://"^filename)
 
 
 let hostname = lazy (
