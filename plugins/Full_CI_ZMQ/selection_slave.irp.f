70d52c126a208ed1f36fea0ebb05b9aeb8dd5bd5
scemama <scemama@login1.occigen>
2017-05-24 15:24:20 +0200

    Larges messages for 100M

--- plugins/Full_CI_ZMQ/selection_slave.irp.f
+++ plugins/Full_CI_ZMQ/selection_slave.irp.f
@@ -5,7 +5,8 @@ program selection_slave
   END_DOC
 
   read_wf = .False.
-  SOFT_TOUCH read_wf
+  distributed_davidson = .False.
+  SOFT_TOUCH read_wf distributed_davidson
   call provide_everything
   call switch_qp_run_to_master
   call run_wf
@@ -13,7 +14,7 @@ end
 
 subroutine provide_everything
   PROVIDE H_apply_buffer_allocated mo_bielec_integrals_in_map psi_det_generators psi_coef_generators psi_det_sorted_bit psi_selectors n_det_generators n_states generators_bitmask zmq_context
-   PROVIDE pt2_e0_denominator mo_tot_num N_int fragment_count
+  PROVIDE pt2_e0_denominator mo_tot_num N_int fragment_count 
 end
 
 subroutine run_wf
@@ -23,19 +24,21 @@ subroutine run_wf
   integer(ZMQ_PTR), external :: new_zmq_to_qp_run_socket
   integer(ZMQ_PTR) :: zmq_to_qp_run_socket
   double precision :: energy(N_states)
-  character*(64) :: states(1)
+  character*(64) :: states(4)
   integer :: rc, i
   
   call provide_everything
   
   zmq_context = f77_zmq_ctx_new ()
   states(1) = 'selection'
+  states(2) = 'davidson'
+  states(3) = 'pt2'
 
   zmq_to_qp_run_socket = new_zmq_to_qp_run_socket()
 
   do
 
-    call wait_for_states(states,zmq_state,1)
+    call wait_for_states(states,zmq_state,3)
 
     if(trim(zmq_state) == 'Stopped') then
 
@@ -51,9 +54,25 @@ subroutine run_wf
   
       !$OMP PARALLEL PRIVATE(i)
       i = omp_get_thread_num()
-      call selection_slave_tcp(i, energy)
+      call run_selection_slave(0,i,energy)
       !$OMP END PARALLEL
       print *,  'Selection done'
+
+    else if (trim(zmq_state) == 'pt2') then
+
+      ! PT2
+      ! ---
+
+      print *,  'PT2'
+      call zmq_get_psi(zmq_to_qp_run_socket,1,energy,N_states)
+  
+      logical :: lstop
+      lstop = .False.
+      !$OMP PARALLEL PRIVATE(i)
+      i = omp_get_thread_num()
+      call run_pt2_slave(0,i,energy,lstop)
+      !$OMP END PARALLEL
+      print *,  'PT2 done'
 
     endif
 
@@ -61,11 +80,4 @@ subroutine run_wf
 end
 
 
-subroutine selection_slave_tcp(i,energy)
-  implicit none
-  double precision, intent(in) :: energy(N_states)
-  integer, intent(in)            :: i
-
-  call run_selection_slave(0,i,energy)
-end
 
