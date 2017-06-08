d533eb50c105487ac09628f8a860e82c475081bc
Thomas Applencourt <TApplencourt@users.noreply.github.com>
2017-06-08 14:30:39 -0500

    Fix Read Integral (#197)
    * Fix read_integrals
    
    * Fix mo_mono_ints

--- src/Integrals_Monoelec/mo_mono_ints.irp.f
+++ src/Integrals_Monoelec/mo_mono_ints.irp.f
@@ -3,13 +3,24 @@ BEGIN_PROVIDER [ double precision, mo_mono_elec_integral,(mo_tot_num_align,mo_to
   integer                        :: i,j,n,l
   BEGIN_DOC
   ! array of the mono electronic hamiltonian on the MOs basis :
-  ! sum of the kinetic and nuclear electronic potential
+  ! sum of the kinetic and nuclear electronic potential (and pseudo potential if needed)
   END_DOC
   print*,'Providing the mono electronic integrals'
-  do j = 1, mo_tot_num
-    do i = 1, mo_tot_num
-      mo_mono_elec_integral(i,j) = mo_nucl_elec_integral(i,j) + &
-         mo_kinetic_integral(i,j) + mo_pseudo_integral(i,j)
-    enddo
-  enddo
+
+  IF (do_pseudo) THEN
+      do j = 1, mo_tot_num
+      do i = 1, mo_tot_num
+          mo_mono_elec_integral(i,j) = mo_nucl_elec_integral(i,j) + mo_kinetic_integral(i,j) & 
+                                       + mo_pseudo_integral(i,j)
+      enddo
+      enddo
+
+  ELSE
+      do j = 1, mo_tot_num
+      do i = 1, mo_tot_num
+          mo_mono_elec_integral(i,j) = mo_nucl_elec_integral(i,j) + mo_kinetic_integral(i,j)
+      enddo
+      enddo
+
+  END IF
 END_PROVIDER
