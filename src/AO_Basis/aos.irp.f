631ef5b54cbff02f82e9a339f65a961746194cab
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2017-08-15 17:56:23 +0200

    Fixed GPI2

--- src/AO_Basis/aos.irp.f
+++ src/AO_Basis/aos.irp.f
@@ -10,13 +10,10 @@ BEGIN_PROVIDER [ integer, ao_num_align ]
 END_PROVIDER 
 
  BEGIN_PROVIDER [ integer, ao_prim_num_max ]
-&BEGIN_PROVIDER [ integer, ao_prim_num_max_align ]
  implicit none
  ao_prim_num_max = 0
  PROVIDE ezfio_filename
  call ezfio_get_ao_basis_ao_prim_num_max(ao_prim_num_max)
- integer :: align_double
- ao_prim_num_max_align = align_double(ao_prim_num_max)
 END_PROVIDER
 
  BEGIN_PROVIDER [ double precision, ao_coef_normalized, (ao_num_align,ao_prim_num_max) ]
