d6b160053d33e63b88a97ff5b05c1a62d532c0c3
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2017-09-14 18:20:00 +0200

    Manual install of Lapack

--- install/scripts/install_lapack.sh
+++ install/scripts/install_lapack.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/Reference-LAPACK/lapack-release.git
 cd lapack-release
 cp make.inc.example make.inc
-make -j 8 lib
+make -j 8 
 mv librefblas.a liblapack.a libtmglib.a $QP_ROOT/lib
 
 
