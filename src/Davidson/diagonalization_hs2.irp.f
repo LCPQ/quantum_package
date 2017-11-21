ca5f6efc9357ad8eea00956873df749051f942e9
Anthony Scemama <scemama@irsamc.ups-tlse.fr>
2017-11-21 09:24:39 +0100

    Fixed print in Davdison

--- src/Davidson/diagonalization_hs2.irp.f
+++ src/Davidson/diagonalization_hs2.irp.f
@@ -132,21 +132,21 @@ subroutine davidson_diag_hjj_sjj(dets_in,u_in,H_jj,s2_out,energies,dim_in,sze,N_
     + 4.d0*(N_st_diag*itermax)+nproc*(4.d0*N_det_alpha_unique+2.d0*N_st_diag*sze)))/(1024.d0**3)
   call write_double(iunit, r1, 'Memory(Gb)')
   write(iunit,'(A)') ''
-  write_buffer = '===== '
+  write_buffer = '===='
   do i=1,N_st
     write_buffer = trim(write_buffer)//' ================ =========== ==========='
   enddo
-  write(iunit,'(A)') trim(write_buffer)
-  write_buffer = ' Iter'
+  write(iunit,'(A)') write_buffer(1:6+41*N_states)
+  write_buffer = 'Iter'
   do i=1,N_st
-    write_buffer = trim(write_buffer)//'      Energy          S^2      Residual      '
+    write_buffer = trim(write_buffer)//'       Energy          S^2       Residual '
   enddo
-  write(iunit,'(A)') trim(write_buffer)
-  write_buffer = '===== '
+  write(iunit,'(A)') write_buffer(1:6+41*N_states)
+  write_buffer = '===='
   do i=1,N_st
     write_buffer = trim(write_buffer)//' ================ =========== ==========='
   enddo
-  write(iunit,'(A)') trim(write_buffer)
+  write(iunit,'(A)') write_buffer(1:6+41*N_states)
   
 
   allocate(                                                          &
