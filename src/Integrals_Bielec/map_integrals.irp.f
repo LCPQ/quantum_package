bfb0c8732c03812fd77582cce81454ba006c8267
Kevin Gasperich <kgasperich@users.noreply.github.com>
2018-05-22 13:51:43 -0500

    corrected conditional related to data types (#234)
    difference between out_val and tmp_val is integral_kind (instead of key_kind)

--- src/Integrals_Bielec/map_integrals.irp.f
+++ src/Integrals_Bielec/map_integrals.irp.f
@@ -447,7 +447,7 @@ subroutine get_mo_bielec_integrals(j,k,l,sze,out_val,map)
     call bielec_integrals_index(i,j,k,l,hash(i))
   enddo
   
-  if (key_kind == 8) then
+  if (integral_kind == 8) then
     call map_get_many(map, hash, out_val, sze)
   else
     call map_get_many(map, hash, tmp_val, sze)
@@ -534,7 +534,7 @@ subroutine get_mo_bielec_integrals_coulomb_ii(k,l,sze,out_val,map)
     call bielec_integrals_index(k,i,l,i,hash(i))
   enddo
   
-  if (key_kind == 8) then
+  if (integral_kind == 8) then
     call map_get_many(map, hash, out_val, sze)
   else
     call map_get_many(map, hash, tmp_val, sze)
@@ -567,7 +567,7 @@ subroutine get_mo_bielec_integrals_exch_ii(k,l,sze,out_val,map)
     call bielec_integrals_index(k,i,i,l,hash(i))
   enddo
   
-  if (key_kind == 8) then
+  if (integral_kind == 8) then
     call map_get_many(map, hash, out_val, sze)
   else
     call map_get_many(map, hash, tmp_val, sze)
